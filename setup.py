a7195a261242103831b7e368a9aca6dca061b6dd
Thomas Merkel <tm@core.io>
2018-08-09 19:45:17 +0200

    Version bump to 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='free_tls_certificates',
-    version='0.1.6',
+    version='0.1.7',
     author=u'Joshua Tauberer',
     author_email=u'jt@occams.info',
     packages = find_packages(),
